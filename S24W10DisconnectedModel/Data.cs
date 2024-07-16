84c0008cd69b746f2e904fa6592e29c4a699b24c
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2024-07-16 08:25:56 -0400

    Add project files.

---
+++ S24W10DisconnectedModel/Data.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S24W10DisconnectedModel
+{
+    public class Data
+    {
+        private static string connectionStr = @"Data Source=(LocalDB)\MSSQLLocalDB;Initial Catalog=Northwind;Integrated Security=True";
+
+        public static string ConnectionString { get { return connectionStr; } }
+    }
+}
