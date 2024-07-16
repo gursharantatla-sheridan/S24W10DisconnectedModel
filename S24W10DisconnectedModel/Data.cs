6d8ba4b6e4e6ffd234920ab0d5d34424f0986f85
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2024-07-16 08:43:56 -0400

    get all products

--- S24W10DisconnectedModel/Data.cs
+++ S24W10DisconnectedModel/Data.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
 
 namespace S24W10DisconnectedModel
 {
@@ -11,5 +13,20 @@ namespace S24W10DisconnectedModel
         private static string connectionStr = @"Data Source=(LocalDB)\MSSQLLocalDB;Initial Catalog=Northwind;Integrated Security=True";
 
         public static string ConnectionString { get { return connectionStr; } }
+
+        public DataTable GetAllProducts()
+        {
+            SqlConnection conn = new SqlConnection(ConnectionString);
+
+            string query = "select ProductID, ProductName, UnitPrice, UnitsInStock from Products";
+            SqlDataAdapter adp = new SqlDataAdapter(query, conn);
+
+            DataSet ds = new DataSet();
+
+            adp.Fill(ds, "Products");
+
+            DataTable tbl = ds.Tables["Products"];
+            return tbl;
+        }
     }
 }
