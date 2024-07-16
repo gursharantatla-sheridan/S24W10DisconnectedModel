99ecec52af1b35259c44aca1ee7ea2065c44a91a
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2024-07-16 09:34:18 -0400

    crud operation, get all products

---
+++ S24W10DisconnectedModel/Crud.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace S24W10DisconnectedModel
+{
+    public class Crud
+    {
+        private SqlConnection conn;
+        private SqlDataAdapter adp;
+        private SqlCommandBuilder cmdBuilder;
+        private DataSet ds;
+        private DataTable tbl;
+
+        public Crud()
+        {
+            string query = "select ProductID, ProductName, UnitPrice, UnitsInStock from Products";
+
+            conn = new SqlConnection(Data.ConnectionString);
+            adp = new SqlDataAdapter(query, conn);
+            cmdBuilder = new SqlCommandBuilder(adp);
+        }
+
+        private void FillDataSet()
+        {
+            ds = new DataSet();
+            adp.Fill(ds);
+
+            tbl = ds.Tables[0];
+        }
+
+        public DataTable GetAllProducts()
+        {
+            FillDataSet();
+            return tbl;
+        }
+    }
+}
